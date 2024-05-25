83b00c13b6a25f15cd0bf7d7ca06b90b301b18cd
havanchau <chauhvc2.dy@gmail.com>
2024-05-25 23:23:48 +0700

    update: change time of jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -17,7 +17,7 @@ import { AuthController } from './auth.controller';
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: '60m' },
+        signOptions: { expiresIn: '30d' },
       }),
     }),
   ],
